a4a2bf382d9e1a0058fe14a365f65f5a8b4e917e
serhatates <serhat.ates@savronik.com.tr>
2021-11-07 11:43:57 +0300

    added two more test cases

--- src/move.spec.ts
+++ src/move.spec.ts
@@ -40,7 +40,70 @@ describe('move', () => {
       },
     ];
 
-    expect(move(list, '4', '6')).toStrictEqual(result);
+    expect(move(list, '4  ', '  6')).toStrictEqual(result);
+  });
+
+  it('moves given file to another folder', () => {
+    const list = [
+      {
+        id: '0',
+        name: 'Folder 4',
+        files: [{ id: '9', name: 'File 7' }],
+      },
+      {
+        id: '1',
+        name: 'Folder 1',
+        files: [
+          { id: '2', name: 'File 1' },
+          { id: '3', name: 'File 2' },
+          { id: '4', name: 'File 3' },
+          { id: '5', name: 'File 4' },
+        ],
+      },
+      {
+        id: '6',
+        name: 'Folder 2',
+        files: [{ id: '7', name: 'File 5' }],
+      },
+      {
+        id: '7',
+        name: 'Folder 3',
+        files: [{ id: '8', name: 'File 6' }],
+      }
+    ];
+
+    const result = [
+      {
+        id: '0',
+        name: 'Folder 4',
+        files: [
+          { id: '9', name: 'File 7' },
+          { id: '8', name: 'File 6' }
+        ]
+      },
+      {
+        id: '1',
+        name: 'Folder 1',
+        files: [
+          { id: '2', name: 'File 1' },
+          { id: '3', name: 'File 2' },
+          { id: '4', name: 'File 3' },
+          { id: '5', name: 'File 4' }
+        ]
+      },
+      {
+        id: '6',
+        name: 'Folder 2',
+        files: [{ id: '7', name: 'File 5' }]
+      },
+      {
+        id: '7',
+        name: 'Folder 3',
+        files: []
+      }
+    ]
+
+    expect(move(list, '8', '0')).toStrictEqual(result);
   });
 
   it('throws error if given source is not a file', () => {
@@ -67,5 +130,18 @@ describe('move', () => {
     ];
 
     expect(() => move(list, '2', '4')).toThrow('You cannot specify a file as the destination');
+  });
+
+  it('throws error if given args are invalid', () => {
+    const list = [
+      {
+        id: '1',
+        name: 'Folder 1',
+        files: [{ id: '2', name: 'File 1' }],
+      },
+      { id: '3', name: 'Folder 2', files: [{ id: '4', name: 'File 2' }] },
+    ];
+
+    expect(() => move(list, ' ', '4a')).toThrow('Invalid arguments');
   });
 });
